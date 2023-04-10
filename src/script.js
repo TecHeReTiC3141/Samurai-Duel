784bf9037a28a6467937f0efcbb5c1a86636f3cd
TecHeReTiC <talex73@mail.ru>
2023-04-11 00:02:49 +0500

    Enemy class and movement have been added

--- src/script.js
+++ src/script.js
@@ -9,15 +9,27 @@ window.addEventListener('resize', () => {
     canvas.height = window.innerHeight;
 });
 
+const keys = {
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+    ArrowRight: {
+        pressed: false,
+    },
+    ArrowLeft: {
+        pressed: false,
+    },
+}
+
 window.addEventListener('keydown', ev => {
-    let key = ev.key.toLowerCase();
-    switch (key) {
-        case 'a':
-            player.velocity.x = -5;
-            break;
-        case 'd':
-            player.velocity.x = 5;
-            break;
+    console.log(ev.key);
+    if (ev.key in keys) {
+        keys[ev.key].pressed = true;
+    }
+    switch (ev.key) {
         case 'w':
             player.jump();
             break;
@@ -25,25 +37,17 @@ window.addEventListener('keydown', ev => {
 });
 
 window.addEventListener('keyup', ev => {
-    let key = ev.key.toLowerCase();
-    switch (key) {
-        case 'a':
-            player.velocity.x = 0;
-            break;
-        case 'd':
-            player.velocity.x = 0;
-            break;
-        case 'w':
-            player.jump();
-            break;
+    if (ev.key in keys) {
+        keys[ev.key].pressed = false;
     }
+
 });
 
 class Sprite {
 
     width = 150;
     height = 250;
-
+    color = 'black';
     gravity = .125;
 
     constructor({position, velocity}) {
@@ -53,7 +57,7 @@ class Sprite {
 
     draw() {
         c.beginPath();
-        c.fillStyle = 'green';
+        c.fillStyle = this.color;
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
 
     }
@@ -67,8 +71,9 @@ class Sprite {
             this.velocity.y = Math.min(this.velocity.y + this.gravity, 5);
         }
 
-        player.position.x = Math.min(Math.max(player.position.x +
-            player.velocity.x, 0), canvas.width - this.width);
+        this.position.x = Math.min(Math.max(this.position.x +
+            this.velocity.x, 0), canvas.width - this.width);
+
     }
 
     jump() {
@@ -78,9 +83,50 @@ class Sprite {
     }
 }
 
-let player = new Sprite({
+class Player extends Sprite {
+    color = 'green';
+
+    update() {
+        super.update();
+        this.velocity.x = 0;
+        if (keys.a.pressed && keys.d.pressed) {
+            this.velocity.x = 0;
+        } else if (keys.d.pressed) {
+            this.velocity.x = 5;
+        } else if (keys.a.pressed) {
+            this.velocity.x = -5;
+        }
+    }
+}
+
+class Enemy extends Sprite {
+    color = 'red';
+
+    update() {
+        super.update();
+        this.velocity.x = 0;
+        if (keys.ArrowRight.pressed && keys.ArrowLeft.pressed) {
+            this.velocity.x = 0;
+        } else if (keys.ArrowRight.pressed) {
+            this.velocity.x = 5;
+        } else if (keys.ArrowLeft.pressed) {
+            this.velocity.x = -5;
+        }
+    }
+}
+
+let player = new Player({
     position: {
         x: 150,
+        y: 150,
+    }, velocity: {
+        x: 0,
+        y: 10,
+    }
+});
+let enemy = new Enemy({
+    position: {
+        x: 450,
         y: 150,
     }, velocity: {
         x: 0,
@@ -97,6 +143,7 @@ function animate() {
 
 function draw() {
     player.update();
+    enemy.update();
     c.fillStyle = 'black';
     c.fillRect(0, canvas.height - ground_level, canvas.width, ground_level);
 }
